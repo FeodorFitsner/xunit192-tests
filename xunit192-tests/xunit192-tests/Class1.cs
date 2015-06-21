48754cb6dd06586fb74618a9e8f18279b7330c6b
Feodor Fitsner <feodor@fitsner.com>
2015-06-21 10:15:19 -0700

    First checkin

---
+++ xunit192-tests/xunit192-tests/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace xunit192_tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
